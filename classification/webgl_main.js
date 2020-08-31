74a7f7f545bc199f9302b37748b8bc819dd3b4e3
terryky <terryky1220@gmail.com>
2020-08-31 19:31:40 +0900

    refactoring

--- classification/webgl_main.js
+++ classification/webgl_main.js
@@ -7,6 +7,7 @@
 let s_debug_log;
 let s_rtarget_main;
 let s_rtarget_feed;
+let s_rtarget_src;
 
 function init_stats ()
 {
@@ -43,14 +44,14 @@ function generate_input_image (gl, texid, win_w, win_h)
     return buf_rgb;
 }
 
-function render_classification_result (gl, predictions)
+function render_classification_result (gl, predictions, win_w, win_h)
 {
     for (let i = 0; i < predictions.length; i ++)
     {
         let col_str  = [1.0, 1.0, 1.0, 1.0];
         let col_blue = [0.0, 0.0, 0.8, 0.8];
         let col_gray = [0.3, 0.3, 0.3, 0.8];
-        let dy = 480 - 22 * (predictions.length - i);
+        let dy = win_h - 22 * (predictions.length - i);
 
         let item = predictions[i];
         let col_bg = item.probability > 0.5 ? col_blue : col_gray;
@@ -59,6 +60,48 @@ function render_classification_result (gl, predictions)
         buf = "[" + i + "]" + item.probability.toFixed(2) + "(" + item.index + ")" + item.class_name;
         dbgstr.draw_dbgstr_ex (gl, buf, 0, dy, 1.0, col_str, col_bg);
     }
+}
+
+/* Adjust the texture size to fit the window size
+ *
+ *                      Portrait
+ *     Landscape        +------+
+ *     +-+------+-+     +------+
+ *     | |      | |     |      |
+ *     | |      | |     |      |
+ *     +-+------+-+     +------+
+ *                      +------+
+ */
+function
+generate_squared_src_image (gl, texid, src_w, src_h, win_w, win_h)
+{
+    let win_aspect = win_w / win_h;
+    let tex_aspect = src_w / src_h;
+    let scale;
+    let scaled_w, scaled_h;
+    let offset_x, offset_y;
+
+    if (win_aspect > tex_aspect)
+    {
+        scale = win_h / src_h;
+        scaled_w = scale * src_w;
+        scaled_h = scale * src_h;
+        offset_x = (win_w - scaled_w) * 0.5;
+        offset_y = 0;
+    }
+    else
+    {
+        scale = win_w / src_w;
+        scaled_w = scale * src_w;
+        scaled_h = scale * src_h;
+        offset_x = 0;
+        offset_y = (win_h - scaled_h) * 0.5;
+    }
+
+    GLUtil.set_render_target (gl, s_rtarget_src);
+    gl.clearColor (0.0, 0.0, 0.0, 1.0);
+    gl.clear (gl.COLOR_BUFFER_BIT);
+    r2d.draw_2d_texture (gl, texid, offset_x, offset_y, scaled_w, scaled_h, 1)
 }
 
 
@@ -81,13 +124,11 @@ async function startWebGL()
     gl.clear (gl.COLOR_BUFFER_BIT);
 
     const camtex = GLUtil.create_camera_texture (gl);
-    const imgtex = GLUtil.create_image_texture2 (gl, "pakutaso_strawberry.jpg");
-    let texid = imgtex.texid;
+    //const camtex = GLUtil.create_video_texture (gl, "./assets/pexels_fruits.mp4");
+    const imgtex = GLUtil.create_image_texture2 (gl, "./assets/pakutaso_strawberry.jpg");
 
     let win_w = canvas.clientWidth;
     let win_h = canvas.clientHeight;
-    let cam_w = 0;
-    let cam_h = 0;
 
     r2d.init_2d_render (gl, win_w, win_h);
     init_dbgstr (gl, win_w, win_h);
@@ -100,9 +141,12 @@ async function startWebGL()
 
     s_rtarget_main = GLUtil.create_render_target (gl, win_w, win_h, 0);
     s_rtarget_feed = GLUtil.create_render_target (gl, win_w, win_w, 1);
+    s_rtarget_src  = GLUtil.create_render_target (gl, win_w, win_w, 1);
 
+    /* stop loading spinner */
+    const spinner = document.getElementById('loading');
+    spinner.classList.add('loaded');
 
-    let count = 0;
     let prev_time_ms = performance.now();
     async function render (now)
     {
@@ -115,39 +159,48 @@ async function startWebGL()
 
         stats.begin();
 
+        let src_w = imgtex.image.width;
+        let src_h = imgtex.image.height;
+        let texid = imgtex.texid;
         if (GLUtil.is_camera_ready(camtex))
         {
             GLUtil.update_camera_texture (gl, camtex);
-            cam_w = camtex.video.videoWidth;
-            cam_h = camtex.video.videoHeight;
+            src_w = camtex.video.videoWidth;
+            src_h = camtex.video.videoHeight;
             texid = camtex.texid;
         }
 
+        generate_squared_src_image (gl, texid, src_w, src_h, win_w, win_h);
+        texid = s_rtarget_src.texid;
+
+        /* --------------------------------------- *
+         *  invoke TF.js
+         * --------------------------------------- */
         let feed_image = generate_input_image (gl, texid, win_w, win_h);
 
-        /* --------------------------------- *
-         *  invoke TF.js
-         * --------------------------------- */
-        let time_invoke_start = performance.now();
+        let time_invoke0_start = performance.now();
         let predictions = await invoke_classification (feed_image);
-        let time_invoke = performance.now() - time_invoke_start;
+        let time_invoke0 = performance.now() - time_invoke0_start;
 
-        /* --------------------------------- *
-         *  render results
-         * --------------------------------- */
+        /* --------------------------------------- *
+         *  render scene
+         * --------------------------------------- */
         GLUtil.set_render_target (gl, s_rtarget_main);
         gl.clear (gl.COLOR_BUFFER_BIT);
 
         r2d.draw_2d_texture (gl, texid, 0, 0, win_w, win_h, 0)
 
-        render_classification_result (gl, predictions);
+        render_classification_result (gl, predictions, win_w, win_h);
 
+        /* --------------------------------------- *
+         *  post process
+         * --------------------------------------- */
         pmeter.draw_pmeter (gl, 0, 40);
 
         let str = "Interval: " + interval_ms.toFixed(1) + " [ms]";
         dbgstr.draw_dbgstr (gl, str, 10, 10);
 
-        str = "TF.js   : " + time_invoke.toFixed(1)  + " [ms]";
+        str = "TF.js   : " + time_invoke0.toFixed(1)  + " [ms]";
         dbgstr.draw_dbgstr (gl, str, 10, 10 + 22 * 1);
 
         stats.end();
